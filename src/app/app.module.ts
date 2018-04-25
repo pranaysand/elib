4a40874e1180fe25c10e6306156aa81f319ccd28
Dexters Home <pranay.livingstone@gmail.com>
2018-04-24 19:38:50 -0500

    project elib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+   
   ],
   imports: [
     BrowserModule
