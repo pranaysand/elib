a0f76e08bd256ce2ce2684f347dc7d8dfa37f37e
DEXTERS-HOME\livings <pranay.livingstone@gmail.com>
2018-04-24 20:13:55 -0500

    Added Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+//Bootstrap NGX
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 
 
@@ -15,7 +17,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
